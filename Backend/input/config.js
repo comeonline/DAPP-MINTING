65f9983fbae944a33854c881db7c0327267f3de6
Perreve <perreve.enzo@gmail.com>
2021-09-05 12:13:11 +0200

    Ajout de l'URL server metadata + création nouveau NFT pour test

--- Backend/input/config.js
+++ Backend/input/config.js
@@ -3,10 +3,10 @@ const width = 1000;
 const height = 1000;
 const dir = __dirname;
 const description = "This is an NFT made by the coolest generative code.";
-const baseImageUri = "https://hashlips/nft";
+const baseImageUri = "https://garagedelarochette.fr/metadata/";
 const startEditionFrom = 1;
-const endEditionAt = 10;
-const editionSize = 10;
+const endEditionAt = 100;
+const editionSize = 100;
 const rarityWeights = [
   {
     value: "super_rare",
